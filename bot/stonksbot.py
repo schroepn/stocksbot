dbd848c6113babb8662799b276c526f76fa40c4e
pizzadeliverymen <50088322+pizzadeliverymen@users.noreply.github.com>
2021-02-01 12:40:03 -0500

    Update stonksbot.py

--- bot/stonksbot.py
+++ bot/stonksbot.py
@@ -20,13 +20,20 @@ async def on_message(message):
         return
 
     if message.content.startswith('$stonks'):
-        while True:
-            ticker = message.content.split('$stonks ',1)[1]
-            await message.channel.send('Getting stonks, this may take some time...')
-            result = await OHLC.fetch(ticker,interval=Interval.HOUR,history=History.DAY)
-            price = str(result['meta']['regularMarketPrice'])
-            await message.channel.send('TODAYS DATA FOR ' + ticker.upper() + '\nCurrent Stock Price: ' + price + '\nToday\'s High: ' + str(result['candles'][0]['high']) + '\nToday\'s Low: ' + str(result['candles'][0]['low']))
-            break
+        
+        #gets the ticker from the input
+        ticker = message.content.split('$stonks ',1)[1]
+        #sends message saying it will take time so peopel don't get angery
+        await message.channel.send('Getting stonks, this may take some time...')
+        #gets the ticker data using yahoo_finance_async, data is every 15 minutes for the past day
+        result = await OHLC.fetch(ticker,interval=Interval.FIFTEEN_MINUTE,history=History.DAY)
+        print('got the ticker data')
+        #gets the current price for a share
+        price = str(result['meta']['regularMarketPrice'])
+        print('got the price at: ' + str(price))
+        #sends the message to discord
+        await message.channel.send('TODAYS DATA FOR ' + ticker.upper() + '\nCurrent Stock Price: ' + price + '\nToday\'s High: ' + str(result['candles'][0]['high']) + '\nToday\'s Low: ' + str(result['candles'][0]['low']))
+        
         
 
 client.run(TOKEN)

667c835ef15204666815b133b0d56477badc14cd
pizzadeliverymen <50088322+pizzadeliverymen@users.noreply.github.com>
2021-02-02 11:11:07 -0500

    Update stonksbot.py

--- bot/stonksbot.py
+++ bot/stonksbot.py
@@ -29,16 +29,16 @@ async def on_message(message):
             #gets the ticker data using yahoo_finance_async, data is every 15 minutes for the past day
             result = await OHLC.fetch(ticker,interval=Interval.DAY,history=History.DAY)
             print('got the ticker data')
-            await edit_message(sentMessage.id,'Getting stonks, this may take some time..')
+            await Message.edit(sentMessage.id,'Getting stonks, this may take some time..')
             #gets the current price for a share
             price = result['meta']['regularMarketPrice']
             print('got the price at: ' + str(price))
             print('day price is: ' + str(result['meta']['regularMarketPrice']))
-            await edit_message(sentMessage.id,'Getting stonks, this may take some time...')
+            await Messageedit(sentMessage.id,'Getting stonks, this may take some time...')
             #formats the message
             toSend = 'Today\'s Data for {stock}\nCurrent Stock Price: {pps:.3f}\nToday\'s High: {high:.3f}\nToday\'s Low: {low:.3f}'
             #sends the message to discord
-            await edit_message(sentMessage.id,toSend.format(stock=ticker,pps=price,high=result['candles'][0]['high'],low=result['candles'][0]['low']))
+            await Messageedit(sentMessage.id,toSend.format(stock=ticker,pps=price,high=result['candles'][0]['high'],low=result['candles'][0]['low']))
         except api.APIError:
             await message.channel.send('That Stonk Doesn\'t Exist You Dummy')
         

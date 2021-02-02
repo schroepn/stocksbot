b53e890acbea9005639286330b81a55be1a715c5
pizzadeliverymen <50088322+pizzadeliverymen@users.noreply.github.com>
2021-02-02 12:52:14 -0500

    made embeds more aesthetically pleasing

--- bot/stonksbot.py
+++ bot/stonksbot.py
@@ -36,9 +36,9 @@ async def on_message(message):
             print('day price is: ' + str(result['meta']['regularMarketPrice']))
             #formats the message into an embed
             stonkInfoEmbed = discord.Embed(title='Today\'s Data for {stock}'.format(stock=ticker))
-            stonkInfoEmbed.add_field(name='Current Price:',value='{pps:.3f}'.format(pps=price))
-            stonkInfoEmbed.add_field(name='Today\'s High:',value='{high:.3f}'.format(high=result['candles'][0]['high']))
-            stonkInfoEmbed.add_field(name='Today\'s Low:',value='{low:.3f}'.format(low=result['candles'][0]['low']))
+            stonkInfoEmbed.add_field(name='Current Price:',value='{pps:.3f}'.format(pps=price),,inline=False)
+            stonkInfoEmbed.add_field(name='Today\'s High:',value='{high:.3f}'.format(high=result['candles'][0]['high']),inline=False)
+            stonkInfoEmbed.add_field(name='Today\'s Low:',value='{low:.3f}'.format(low=result['candles'][0]['low']),inline=False)
             #sends the message to discord
             await sentMessage.edit(embed=stonkInfoEmbed)
         except api.APIError:

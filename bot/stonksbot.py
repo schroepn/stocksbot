fecfd131755ceb14efb967d5811036a0c9cafba2
pizzadeliverymen <50088322+pizzadeliverymen@users.noreply.github.com>
2021-02-01 22:37:36 -0500

    Update stonksbot.py

--- bot/stonksbot.py
+++ bot/stonksbot.py
@@ -20,22 +20,25 @@ async def on_message(message):
         return
 
     if message.content.startswith('$stonks'):
+        try:
         
-        #gets the ticker from the input
-        ticker = message.content.split('$stonks ',1)[1]
-        #sends message saying it will take time so peopel don't get angery
-        await message.channel.send('Getting stonks, this may take some time...')
-        #gets the ticker data using yahoo_finance_async, data is every 15 minutes for the past day
-        result = await OHLC.fetch(ticker,interval=Interval.DAY,history=History.DAY)
-        print('got the ticker data')
-        #gets the current price for a share
-        price = result['meta']['regularMarketPrice']
-        print('got the price at: ' + str(price))
-        print('day price is: ' + str(result['meta']['regularMarketPrice']))
-        #formats the message
-        toSend = 'Today\'s Data for {stock}\nCurrent Stock Price: {pps:.3f}\nToday\'s High: {high:.3f}\nToday\'s Low: {low:.3f}'
-        #sends the message to discord
-        await message.channel.send(toSend.format(stock=ticker,pps=price,high=result['candles'][0]['high'],low=result['candles'][0]['low']))
+            #gets the ticker from the input
+            ticker = message.content.split('$stonks ',1)[1]
+            #sends message saying it will take time so peopel don't get angery
+            await message.channel.send('Getting stonks, this may take some time...')
+            #gets the ticker data using yahoo_finance_async, data is every 15 minutes for the past day
+            result = await OHLC.fetch(ticker,interval=Interval.DAY,history=History.DAY)
+            print('got the ticker data')
+            #gets the current price for a share
+            price = result['meta']['regularMarketPrice']
+            print('got the price at: ' + str(price))
+            print('day price is: ' + str(result['meta']['regularMarketPrice']))
+            #formats the message
+            toSend = 'Today\'s Data for {stock}\nCurrent Stock Price: {pps:.3f}\nToday\'s High: {high:.3f}\nToday\'s Low: {low:.3f}'
+            #sends the message to discord
+            await message.channel.send(toSend.format(stock=ticker,pps=price,high=result['candles'][0]['high'],low=result['candles'][0]['low']))
+        catch(yahoo_finance_async.api.APIError):
+            await message.channel.send('That Stonk Doesn\'t Exist You Dummy')
         
         
 
